8e4b2c5b13c1a081f95e12022222c3b5a348cfba
Felippe Guedes <felippegustavo1@outlook.com>
2026-05-28 15:23:55 -0300

    test(painel): split teste em server wrapper + client component

---
+++ src/app/painel/teste/client.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function TesteClient() {
+  return <div>Teste OK (server wrapper)</div>;
+}
--- src/app/painel/teste/page.tsx
+++ src/app/painel/teste/page.tsx
@@ -1,4 +1,11 @@
-"use client";
-export default function Teste() {
-  return <div>Teste OK</div>;
+import { headers } from "next/headers";
+import TesteClient from "./client";
+
+export const dynamic = "force-dynamic";
+export const dynamicParams = true;
+export const revalidate = 0;
+
+export default function Page() {
+  headers();
+  return <TesteClient />;
 }
